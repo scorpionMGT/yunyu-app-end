f4002996e26c910890f9c1f2b77fe78ccf0189e7
maoguotao <v_guotao.mao@fowo.com>
2021-11-05 16:48:47 +0800

    perf: 更新

--- config/config.prod.js
+++ config/config.prod.js
@@ -6,7 +6,7 @@ exports.mysql = {
     host: '127.0.0.1',
     port: '3306',
     user: 'root',
-    password: 'Mao!123456',
+    password: '123456',
     database: 'yunyu',
   },
   // load into app, default true
