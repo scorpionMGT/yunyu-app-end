bca50890cebe31dbbb963aa647bef9d7c20f2e71
毛国涛 <1142656486@qq.com>
2022-05-25 10:21:19 +0800

    城市代理商页面

--- config/config.test.js
+++ config/config.test.js
@@ -6,7 +6,7 @@ exports.mysql = {
     host: '127.0.0.1',
     port: '3306',
     user: 'root',
-    password: '123456',
+    password: 'Mao!123456',
     database: 'yunyu',
   },
   // load into app, default true
